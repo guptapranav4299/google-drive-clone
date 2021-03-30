12fe91275b12ac1c047dc6eb06fe3fbf6f5f3ebd
Pranav Gupta <guptapranav4299@gmail.com>
2021-03-30 14:43:58 +0530

    complete project

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_objectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+const storage = firebase.storage()
+const db = firebaseApp.firestore()
+
+export { auth, provider, db, storage }
